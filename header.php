ed6b8f632d2c3df039155ce777a6d9a8540235a9
MymenTech <mymentech@gmail.com>
2018-11-30 23:40:00 +0600

    Updated header color and bg

--- header.php
+++ header.php
@@ -46,14 +46,15 @@ $container = get_theme_mod( 'presise_container_type' );
                              style="background-image: url(<?php echo esc_attr( $slide['slide_image'] ) ?>);">
                             <div class="hero-text animated fadeInUpBig slow">
                                 <h3 class="hero-text">
-		                            <?php echo esc_html( $slide['slide_text'] ) ?>
+									<?php echo esc_html( $slide['slide_text'] ) ?>
                                 </h3>
                             </div>
                         </div>
 					<?php endforeach; ?>
                 </div>
             </div>
-            <a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'presise' ); ?></a>
+            <a class="skip-link sr-only sr-only-focusable"
+               href="#content"><?php esc_html_e( 'Skip to content', 'presise' ); ?></a>
             <nav class="navbar navbar-expand-md navbar-dark bg-transparent">
 
 				<?php if ( 'container' == $container ) : ?>
@@ -108,60 +109,60 @@ $container = get_theme_mod( 'presise_container_type' );
             </nav><!-- .site-navigation -->
 		<?php else: ?>
 
-        <a class="skip-link sr-only sr-only-focusable"
-           href="#"><?php esc_html_e( 'Skip to content', 'presise' ); ?></a>
-        <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+            <a class="skip-link sr-only sr-only-focusable"
+               href="#"><?php esc_html_e( 'Skip to content', 'presise' ); ?></a>
+            <nav class="navbar navbar-expand-md navbar-dark bg-white">
 
-			<?php if ( 'container' == $container ) : ?>
-            <div class="container">
+				<?php if ( 'container' == $container ) : ?>
+                <div class="container">
+					<?php endif; ?>
+
+                    <!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
+
+				<?php if ( is_front_page() && is_home() ) : ?>
+
+                    <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                                                     title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+                                                     itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+				<?php else : ?>
+
+                    <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                       title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+                       itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
 				<?php endif; ?>
 
-                <!-- Your site title as branding in the menu -->
-				<?php if ( ! has_custom_logo() ) { ?>
 
-			<?php if ( is_front_page() && is_home() ) : ?>
+					<?php } else {
+						the_custom_logo();
+					} ?><!-- end custom logo -->
 
-                <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                                                 title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-                                                 itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                            aria-controls="navbarNavDropdown" aria-expanded="false"
+                            aria-label="<?php esc_attr_e( 'Toggle navigation', 'presise' ); ?>">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-			<?php else : ?>
-
-                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                   title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-                   itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
+                    <!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav ml-auto',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 2,
+							'walker'          => new Presise_WP_Bootstrap_Navwalker(),
+						)
+					); ?>
+					<?php if ( 'container' == $container ) : ?>
+                </div><!-- .container -->
 			<?php endif; ?>
 
+            </nav><!-- .site-navigation -->
 
-				<?php } else {
-					the_custom_logo();
-				} ?><!-- end custom logo -->
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                        aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="<?php esc_attr_e( 'Toggle navigation', 'presise' ); ?>">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Presise_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-				<?php if ( 'container' == $container ) : ?>
-            </div><!-- .container -->
 		<?php endif; ?>
-
-        </nav><!-- .site-navigation -->
-
-        <?php endif; ?>
     </div><!-- #wrapper-navbar end -->
